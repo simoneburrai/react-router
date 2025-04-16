dc96817a7366193ff9ba6c1c8b9103f638a87d3e
simoneburrai <burrai.s98@gmail.com>
2025-04-16 13:54:05 +0200

    feat: axios get with use effect, called posts and painted on page, modified css of posts

--- src/pages/PostList.jsx
+++ src/pages/PostList.jsx
@@ -1,16 +1,31 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-const apiUrl = "";
+const apiUrl = "https://jsonplaceholder.typicode.com/posts";
 
 const PostList = () => {
     const [posts, setPosts] = useState([]);
     useEffect(() => {
-        axios.get
+        axios.get(apiUrl)
+            .then(res => setPosts(res.data))
     }, [])
 
+    console.log(posts)
 
-    return <main id="posts">Lista dei post</main>
+
+
+    return <main id="posts">
+        <h2>Lista dei Post</h2>
+        <div className="posts-container">
+            {posts.map(post => {
+                return <div className="post" id={post.id}>
+                    <h4>{post.title}</h4>
+                    <p>{post.body}</p>
+                </div>
+
+            })}
+        </div>
+    </main>
 }
 
 export default PostList;
