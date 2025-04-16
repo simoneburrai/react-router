492ccca0e8ac4ced89f0865a94568d22121bfcaa
simoneburrai <burrai.s98@gmail.com>
2025-04-16 12:58:21 +0200

    feat: imported broswerrouter, routes and route on App.jsx, called paths for 3 pages created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage"
+import Chisiamo from "./pages/Chisiamo"
+import PostList from "./pages/PostList"
 
 
 function App() {
 
-  return (
-    <>
-      Siamo ON
-    </>
-  )
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/chi-siamo" element={<Chisiamo />} />
+      <Route path="/posts" element={<PostList />} />
+    </Routes>
+
+  </BrowserRouter>
 }
 
 export default App
