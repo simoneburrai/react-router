3d2f80ebaab3b97a89cdb3650a067ba5e641b8a9
simoneburrai <burrai.s98@gmail.com>
2025-04-16 13:11:27 +0200

    feat: footer modified

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -8,7 +8,17 @@ const DefaultLayout = () => {
             <NavBar />
         </header>
         <Outlet />
-        <footer></footer>
+        <footer>
+            <h3>Info Utili</h3>
+            <ul className="links-footer">
+                <li><a href="">Informazioni di Copyright </a></li>
+                <li><a href="">Privacy Policy</a></li>
+                <li><a href="">Termini e Condizioni</a></li>
+                <li><a href="">Informazioni di Copyright</a></li>
+                <li><a href="">Contatti</a></li>
+                <li><a href="">FAQ</a></li>
+            </ul>
+        </footer>
     </>
 }
 
